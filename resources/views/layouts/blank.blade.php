57c9e81353ff49a2fb4acd015b58c385f2517bca
Rodrigues Filho <ednofco@gmail.com>
2017-03-24 14:41:42 -0300

    Consertei alguns problemas com inclusão de js e css

--- resources/views/layouts/blank.blade.php
+++ resources/views/layouts/blank.blade.php
@@ -1,48 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-        <!-- Meta, title, CSS, favicons, etc. -->
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Meta, title, CSS, favicons, etc. -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Gentellela Alela! | </title>
+  <title>Gentellela Alela! | </title>
 
-        <!-- Bootstrap -->
-        <link href="{{ asset("css/bootstrap.min.css") }}" rel="stylesheet">
-        <!-- Font Awesome -->
-        <link href="{{ asset("css/font-awesome.min.css") }}" rel="stylesheet">
-        <!-- Custom Theme Style -->
-        <link href="{{ asset("css/gentelella.min.css") }}" rel="stylesheet">
+  <!-- Bootstrap -->
+  <link href="{{ asset("css/bootstrap.min.css") }}" rel="stylesheet">
+  <!-- Font Awesome -->
+  <link href="{{ asset("css/font-awesome.min.css") }}" rel="stylesheet">
+  <!-- Custom Theme Style -->
+  <link href="{{ asset("css/gentelella.min.css") }}" rel="stylesheet">
 
-        @stack('stylesheets')
+  <link href="gentelella/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="gentelella/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="gentelella/vendors/nprogress/nprogress.css" rel="stylesheet">
+  <link href="gentelella/build/css/custom.min.css" rel="stylesheet">
 
-    </head>
+  @stack('stylesheets')
 
-    <body class="nav-md">
-        <div class="container body">
-            <div class="main_container">
+</head>
 
-                @include('includes/sidebar')
+<body class="nav-md">
+  <div class="container body">
+    <div class="main_container">
 
-                @include('includes/topbar')
+      @include('includes/sidebar')
 
-                @yield('main_container')
+      @include('includes/topbar')
 
-            </div>
-        </div>
+      @yield('main_container')
 
-        <!-- jQuery -->
-        <script src="{{ asset("js/jquery.min.js") }}"></script>
-        <!-- Bootstrap -->
-        <script src="{{ asset("js/bootstrap.min.js") }}"></script>
-        <!-- Custom Theme Scripts -->
-        <script src="{{ asset("js/gentelella.min.js") }}"></script>
+    </div>
+  </div>
 
-        @stack('scripts')
+  <!-- jQuery -->
+  <!-- <script src="{{ asset("js/jquery.min.js") }}"></script> -->
+  <!-- Bootstrap -->
+  <!-- <script src="{{ asset("js/bootstrap.min.js") }}"></script> -->
+  <!-- Custom Theme Scripts -->
+  <!-- <script src="{{ asset("js/gentelella.min.js") }}"></script> -->
 
-    </body>
+
+  <!-- jQuery -->
+  <script src="gentelella/vendors/jquery/dist/jquery.min.js"></script>
+  <!-- Bootstrap -->
+  <script src="gentelella/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+  <!-- FastClick -->
+  <script src="{{asset("gentelella/vendors/fastclick/lib/fastclick.js")}}"></script>
+  <!-- NProgress -->
+  <script src="gentelella/vendors/nprogress/nprogress.js"></script>
+  <!-- jQuery Smart Wizard -->
+  <script src="{{asset("gentelella/vendors/jQuery-Smart-Wizard/js/jquery.smartWizard.js")}}"></script>
+  <!-- Custom Theme Scripts -->
+  <script src="gentelella/build/js/custom.min.js"></script>
+
+  @stack('scripts')
+
+</body>
 </html>
