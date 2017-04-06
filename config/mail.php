87b34336b493dfbe1af69a82d2b160e70350ddb8
Rodrigues Filho <ednofco@gmail.com>
2017-04-06 15:04:10 -0300

    Atualizei as configurações de email

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'contato@seara.com.br', 'name' => 'Seara Contabilidade'],
 
     /*
     |--------------------------------------------------------------------------
