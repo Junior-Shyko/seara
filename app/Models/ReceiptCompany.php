953fca75ed15a0ff782c309310cca0d729e89cfa
Rodrigues Filho <ednofco@gmail.com>
2017-04-14 15:14:25 -0300

    Data por extenso
    Adicionei um método no model ReceiptCompany para retornar a
    data por extenso.

--- app/Models/ReceiptCompany.php
+++ app/Models/ReceiptCompany.php
@@ -6,17 +6,86 @@ use Illuminate\Database\Eloquent\Model;
 
 class ReceiptCompany extends Model
 {
-    protected $fillable = [
-      'receipt_id_company',
-      'receipt_value',
-      'receipt_extensive_value',
-      'receipt_received_from',
-      'receipt_reference',
-      'receipt_local',
-      'receipt_date',
-      'receipt_emitter',
-      'receipt_document',
-    ];
-    protected $primaryKey = 'receipt_id';
-    protected $table = 'receipt_company';
+  protected $fillable = [
+    'receipt_id_company',
+    'receipt_value',
+    'receipt_extensive_value',
+    'receipt_received_from',
+    'receipt_reference',
+    'receipt_local',
+    'receipt_date',
+    'receipt_emitter',
+    'receipt_document',
+  ];
+  protected $primaryKey = 'receipt_id';
+  protected $table = 'receipt_company';
+
+  /**
+  * The attributes that should be mutated to dates.
+  *
+  * @var array
+  */
+  protected $dates = [
+    'created_at',
+    'updated_at',
+    'deleted_at',
+    'receipt_date'
+  ];
+
+  public function extensiveDate()
+  {
+    $month = '';
+    switch($this->receipt_date->month)
+    {
+      case 1:
+      $month = 'Janeiro';
+      break;
+
+      case 2:
+      $month = 'Fevereiro';
+      break;
+
+      case 3:
+      $month = 'Março';
+      break;
+
+      case 4:
+      $month = 'Abril';
+      break;
+
+      case 5:
+      $month = 'Maio';
+      break;
+
+      case 6:
+      $month = 'Junho';
+      break;
+
+      case 7:
+      $month = 'Julho';
+      break;
+
+      case 8:
+      $month = 'Agosto';
+      break;
+
+      case 9:
+      $month = 'Setembro';
+      break;
+
+      case 10:
+      $month = 'Outubro';
+      break;
+
+      case 11:
+      $month = 'Novembro';
+      break;
+
+      case 12:
+      $month = 'Dezembro';
+      break;
+    }
+
+    return $this->receipt_date->day.' de '.$month.' de '.$this->receipt_date->year;
+  }
 }
--- resources/views/receipt-pdf/receipt_content.blade.php
+++ resources/views/receipt-pdf/receipt_content.blade.php
@@ -4,15 +4,15 @@
     <h3 class="value" style="">VALOR: R$ {{ number_format($receipt->receipt_value,2,',','.') }} </h3>
   </div>
   <div class="first-section">
-    <p><strong>Recebi(emos) de:</strong> {{$receipt->receipt_received_from}}</p>
-    <p><strong>A importância de:</strong> {{$receipt->receipt_extensive_value}}</p>
-    <p><strong>Referente a:</strong> {{$receipt->receipt_reference}}</p>
+    <p><strong>Recebi(emos) de:</strong> {{ $receipt->receipt_received_from }}</p>
+    <p><strong>A importância de:</strong> {{ ucfirst($receipt->receipt_extensive_value) }}</p>
+    <p><strong>Referente a:</strong> {{ $receipt->receipt_reference }}</p>
   </div>
   <div class="second-section">
     <p>Para maior clareza firmo(amos) o presente</p>
   </div>
   <div class="center">
-    <p class="data"> {{ $receipt->receipt_local }}, {{$receipt->receipt_date}}</p>
+    <p class="data"> {{ $receipt->receipt_local }}, {{ $receipt->extensiveDate() }}</p>
     <hr>
-    <p class="contador">{{$receipt->receipt_emitter}} {{$receipt->receipt_document}}</p>
+    <p class="contador">{{ $receipt->receipt_emitter }} {{ $receipt->receipt_document }}</p>
   </div>
