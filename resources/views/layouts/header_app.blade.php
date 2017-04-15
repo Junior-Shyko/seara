eb7f1b5dfa8200142d159d814de17fa7437a01d5
Rodrigues Filho <ednofco@gmail.com>
2017-04-14 23:08:16 -0300

    URL para home
    Adicionei url para a página inicial ao clicar no nome Seara.

--- resources/views/layouts/header_app.blade.php
+++ resources/views/layouts/header_app.blade.php
@@ -8,18 +8,18 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Seara Contabilidade</a>
+      <a class="navbar-brand" href="{{ url('') }}">Seara Contabilidade</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class="active"><a href="{{url('login')}}">Login <span class="sr-only">(current)</span></a></li>
-        
+
       </ul>
-      
+
       <ul class="nav navbar-nav navbar-right">
-       
+
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dúvidas <span class="caret"></span></a>
           <ul class="dropdown-menu">
