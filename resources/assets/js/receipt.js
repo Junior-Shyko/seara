d528f85970ab8471fd68ae1de677dd71b9418cb8
Rodrigues Filho <ednofco@gmail.com>
2017-04-20 09:32:36 -0300

    Tradução do DataTables
    Fiz a tradução da integração com o pacote laravel-datatables.

--- resources/assets/js/receipt.js
+++ resources/assets/js/receipt.js
@@ -85,28 +85,24 @@ $(document).ready(function() {
       { data: 'receipt_local', name: 'receipt_local' },
       { data: 'receipt_date', name: 'receipt_date' },
       {data: 'action', name: 'action', orderable: false, searchable: false}
-    ]
+    ],
+    language: {
+      "lengthMenu": "Exibir _MENU_ recibos por página",
+      "zeroRecords": "Nenhum recibo cadastrado para essa pesquisa",
+      "infoEmpty": "Exibindo 0 de 0 recibos",
+      "emptyTable": "Nenhum redibo cadastrado",
+      "info": "Exibindo página _PAGE_ de _PAGES_",
+      "infoFiltered": "(filtrados de _MAX_ recibos)",
+      "search": "Pesquisar:",
+      "paginate": {
+        "previous": "Anterior",
+        "next": "Próximo",
+        "first": "Primeiro",
+        "last": "Último"
+      }
+    },
+    order: [[0, "desc"]]
   });
-
-  // $('#receipts-table').DataTable({
-  //   "language": {
-  //     "lengthMenu": "Exibir _MENU_ recibos por página",
-  //     "zeroRecords": "Nenhum recibo cadastrado para essa pesquisa",
-  //     "infoEmpty": "Exibindo 0 de 0 recibos",
-  //     "emptyTable": "Nenhum redibo cadastrado",
-  //     "info": "Exibindo página _PAGE_ de _PAGES_",
-  //     "infoFiltered": "(filtrados de _MAX_ recibos)",
-  //     "search": "Pesquisar:",
-  //     "paginate": {
-  //       "previous": "Anterior",
-  //       "next": "Próximo",
-  //       "first": "Primeiro",
-  //       "last": "Último"
-  //     }
-  //   },
-  //   "order": [[ 0, "desc" ]]
-  // } );
-  //
 
   // window.Parsley.addMessage('en', 'required', 'Campo Obrigatório.');
   // initMask();
