85b0eb5ec9be80a7eaf62a54a3f528ef137ccf07
Rodrigues Filho <ednofco@gmail.com>
2017-04-21 08:54:37 -0300

    Comentários e reestruturação
    Adicionei comentários no modal de formulário da tela de recibo.

--- resources/views/modals/receipt/receipt.blade.php
+++ resources/views/modals/receipt/receipt.blade.php
@@ -3,70 +3,83 @@
 
     <!-- Modal -->
     <div class="modal-content">
+
       <!-- Modal Header -->
       <div class="modal-header">
         <h4>Clonar Recibo</h4>
       </div>
 
-      <!-- Formulário  -->
-      <form id="form-receipt" data-parsley-validate="" class="form-horizontal form-label-left" action="{{ url('recibo-empresa') }}" method="POST" autocomplete="off">
 
         <!-- Modal Body -->
         <div class="modal-body">
 
-          {{ csrf_field() }}
+          <!-- Formulário  -->
+          <form id="form-receipt" data-parsley-validate="" class="form-horizontal form-label-left">
 
-          <input type="hidden" name="receipt_id_company" value="{{ $company->company_id }}">
+            {{ csrf_field() }}
 
-          <div class="form-group">
-            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Informações <span class="required">*</span>
-            </label>
-            <div class="col-md-2 col-sm-2 col-xs-12">
-              <input id="receipt_value" name="receipt_value" required placeholder="Valor" class="form-control col-md-7 col-xs-12" type="text">
-            </div>
-            <div class="col-md-2 col-sm-2 col-xs-12">
-              <input name="receipt_local" required placeholder="Local"
-              class="form-control col-md-7 col-xs-12" type="text">
-            </div>
-            <div class="col-md-2 col-sm-2 col-xs-12">
-              <input id="receipt_date" name="receipt_date" required placeholder="Data"
-              class="form-control col-md-7 col-xs-12" type="text">
-            </div>
-          </div>
+            <input type="hidden" id="receipt_id_company" name="receipt_id_company" value="{{ $company->company_id }}">
 
-          <div class="form-group">
-            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Recebemos de <span class="required">*</span>
-            </label>
-            <div class="col-md-6 col-sm-6 col-xs-12">
-              <input name="receipt_received_from" required="required" class="form-control col-md-7 col-xs-12" type="text">
+            <!-- Valor, Local e Data -->
+            <div class="form-group">
+              <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Informações <span class="required">*</span>
+              </label>
+              <!-- Valor -->
+              <div class="col-md-2 col-sm-2 col-xs-12">
+                <input id="receipt_value" name="receipt_value" required placeholder="Valor" class="form-control col-md-7 col-xs-12" type="text">
+              </div>
+              <!-- Local -->
+              <div class="col-md-2 col-sm-2 col-xs-12">
+                <input name="receipt_local" required placeholder="Local"
+                class="form-control col-md-7 col-xs-12" type="text">
+              </div>
+              <!-- Data -->
+              <div class="col-md-2 col-sm-2 col-xs-12">
+                <input id="receipt_date" name="receipt_date" required placeholder="Data"
+                class="form-control col-md-7 col-xs-12" type="text">
+              </div>
             </div>
-          </div>
 
-          <div class="form-group">
-            <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">Referente a <span class="required">*</span></label>
-            <div class="col-md-6 col-sm-6 col-xs-12">
-              <textarea id="message" name="receipt_reference" required class="form-control" name="message"></textarea>
+            <!-- Recebido de -->
+            <div class="form-group">
+              <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Recebemos de <span class="required">*</span>
+              </label>
+              <div class="col-md-6 col-sm-6 col-xs-12">
+                <input name="receipt_received_from" required="required" class="form-control col-md-7 col-xs-12" type="text">
+              </div>
             </div>
-          </div>
 
-          <div class="form-group">
-            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Emitente <span class="required">*</span>
-            </label>
-            <div class="col-md-6 col-sm-6 col-xs-12">
-              <input name="receipt_emitter" required="required"
-              class="form-control col-md-7 col-xs-12" type="text">
+            <!-- Referente a -->
+            <div class="form-group">
+              <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">Referente a <span class="required">*</span></label>
+              <div class="col-md-6 col-sm-6 col-xs-12">
+                <textarea id="message" name="receipt_reference" required class="form-control" name="message"></textarea>
+              </div>
             </div>
-          </div>
 
-          <div class="form-group">
-            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">CNPJ <span class="required">*</span>
-            </label>
-            <div class="col-md-6 col-sm-6 col-xs-12">
-              <input name="receipt_document" required="required"
-              class="form-control col-md-7 col-xs-12" type="text">
+            <!-- Emitente -->
+            <div class="form-group">
+              <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Emitente <span class="required">*</span>
+              </label>
+              <div class="col-md-6 col-sm-6 col-xs-12">
+                <input name="receipt_emitter" required="required"
+                class="form-control col-md-7 col-xs-12" type="text">
+              </div>
             </div>
-          </div>
-        </div>
+
+            <!-- Documento -->
+            <div class="form-group">
+              <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">CNPJ <span class="required">*</span>
+              </label>
+              <div class="col-md-6 col-sm-6 col-xs-12">
+                <input name="receipt_document" required="required"
+                class="form-control col-md-7 col-xs-12" type="text">
+              </div>
+            </div>
+
+          </form> <!-- Fim do form -->
+
+        </div> <!-- Fim do modal-body -->
 
         <!-- Modal Footer -->
         <div class="modal-footer">
@@ -74,8 +87,6 @@
           <button class="btn btn-danger">Salvar</button>
         </div>
 
-      </form>
-
-    </div>
+    </div> <!-- Fim Modal content -->
   </div>
 </div>
