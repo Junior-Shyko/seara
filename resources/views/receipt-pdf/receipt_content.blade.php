8b6152cc8109f3733656d3c2372a37e69b1d8dab
Francisco Edno <ednofco@gmail.com>
2017-08-08 20:27:49 -0300

    ajustes no layout do recibo

--- resources/views/receipt-pdf/receipt_content.blade.php
+++ resources/views/receipt-pdf/receipt_content.blade.php
@@ -3,23 +3,22 @@
     <td colspan="3">
       <img src="{{asset("img/logo/{$company->company_brand_logo}")}}" style="width: 100%; display: block;">
     </td>
-    <td colspan="1"></td>
-    <td colspan="4" class="text-center">
+    {{-- <td colspan="1"></td> --}}
+    {{-- <td colspan="4" class="text-center"> --}}
       {{-- {!! nl2br(e($setting['setting_receipt_header'])) !!} --}}
-    </td>
-    <td colspan="4" class="text-right">
+    {{-- </td> --}}
+    <td colspan="9" class="text-right">
       {!! nl2br(preg_replace('/\*\*\b(.+)\b\*\*/', '<b>$1</b>', e($setting['setting_receipt_header']))) !!}
       <div class="description">
-        {{ ucwords($company->company_addr_street) }}, {{ $company->company_addr_number }}<br>
-        {{ ucwords($company->company_addr_district) }}<br>
+        {{ ucwords($company->company_addr_street) }}, {{ $company->company_addr_number }} - {{ ucwords($company->company_addr_district) }}<br>
         CEP {{ preg_replace('/^(\d{2})(\d{3})(\d{3})$/', '$1.$2-$3', $company->company_addr_cep) }} - {{ ucwords($company->company_addr_city) }} - {{ ucwords($company->company_addr_state) }}<br>
         {{ preg_replace('/^(\d{2})(\d?\d{4})(\d{4})$/', '($1) $2-$3', $company->company_phone) }} / {{ preg_replace('/^(\d{2})(\d?\d{4})(\d{4})$/', '($1) $2-$3', $company->company_mobile) }}<br>
         {{ $setting['setting_receipt_email'] }}<br>
       </div>
     </td>
   </tr>
-  {{-- <tr><td colspan="12" class="offset-5"></td></tr> --}}
   <tr><td colspan="12" class="offset-5"></td></tr>
+  {{-- <tr><td colspan="12" class="offset-5"></td></tr> --}}
   <tr>
     <td colspan="4"></td>
     <td colspan="4" class="text-center">
@@ -33,7 +32,7 @@
       </h4>
     </td>
   </tr>
-  {{-- <tr><td colspan="12" class="offset-5"></td></tr> --}}
+  <tr><td colspan="12" class="offset-5"></td></tr>
   <tr><td colspan="12" class="offset-5"></td></tr>
   <tr>
     <td colspan="3" class="text-bold top-align">Recebi(emos) de: </td>
@@ -47,19 +46,19 @@
     <td colspan="3" class="text-bold top-align">Referente a: </td>
     <td colspan="9" class="top-align">{{ mb_strtoupper($receipt->receipt_reference) }}</td>
   </tr>
-  {{-- <tr><td colspan="12" class="offset-5"></td></tr> --}}
+  <tr><td colspan="12" class="offset-5"></td></tr>
   <tr><td colspan="12" class="offset-5"></td></tr>
   <tr>
     <td colspan="12">Para maior clareza firmo(amos) o presente</td>
   </tr>
-  {{-- <tr><td colspan="12" class="offset-5"></td></tr> --}}
+  <tr><td colspan="12" class="offset-5"></td></tr>
   <tr><td colspan="12" class="offset-5"></td></tr>
   <tr>
     <td colspan="1"></td>
     <td colspan="10" class="text-center">{{ mb_strtoupper($receipt->receipt_local) }}, {{ mb_strtoupper($receipt->extensiveDate()) }}</td>
     <td colspan="1"></td>
   </tr>
-  {{-- <tr><td colspan="12" class="offset-5"></td></tr> --}}
+  <tr><td colspan="12" class="offset-5"></td></tr>
   <tr><td colspan="12" class="offset-5"></td></tr>
   <tr><td colspan="12" class="offset-5"></td></tr>
   <tr>
