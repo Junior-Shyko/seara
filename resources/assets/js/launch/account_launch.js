a3e71c2f9b4453dca4533900af7d289be92ba20b
Junior-Shyko <franciscoanto@gmail.com>
2021-02-15 20:25:58 -0300

    iniciando layout do lançamento no caixa

---
+++ resources/assets/js/launch/account_launch.js
@@ -0,0 +1,88 @@
+$(document).ready(function () {
+    $("#btn-save-account-launch").click(function (e) { 
+        e.preventDefault();
+        var form = $("#form-account-launch").serialize();
+        SearaAjax.post('launch/account/create', form, function( response ){
+			// notify.response(response);
+			// companyTable.reloadTable();
+            new PNotify({
+                title: 'Sucesso',
+                text: response.message,
+                type: response.status,
+                styling: 'bootstrap3'
+            });
+		})
+		.fail(function(jqXHR){
+			notify.response(jqXHR.responseJSON);
+            console.log(jqXHR);
+		})
+		.always(function(){
+			//SearaLoader.hideModal();
+            console.log('hideModal');
+		});
+    });
+});
+
+$(function () {
+    let colunas = [
+        {data: 'accountlaunch_type', name: 'accountlaunch_type'},
+        {data: 'accountlaunch_name', name: 'accountlaunch_name'},
+        {data: 'accountlaunch_history', name: 'accountlaunch_history'},
+        {data: 'created_at', name: 'created_at'},
+        {data: 'accountlaunch_id_user', name: 'accountlaunch_id_user'},
+        {data: 'action', name: 'action', orderable: false, searchable: false, className: 'nowrap'},
+    ];
+    $('#account-launch-table').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: SearaApp.baseURL+'launch/account/all',
+        columns: colunas
+    });
+    // PARA EDIÇÃO DA CONTA DE MOVIMENTO DO CAIXA
+    $('#modalEditAccountLaunch').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var idAccountLaunch = button.data('id') 
+        var nameAccountLaunch = button.data('name') 
+        var typeAccountLaunch = button.data('type') 
+        var typeAccountHistory = button.data('history') 
+        var modal = $(this)
+        modal.find('#modalAccountlaunch_type option[value='+typeAccountLaunch+']').attr('selected','selected');
+        modal.find('#modalAccountlaunch_name').val(nameAccountLaunch);
+        modal.find('#modalAccountlaunch_history').val(typeAccountHistory);
+        modal.find('#modalAccountLaunchId').val(idAccountLaunch);
+    })
+    $('#modalDeleteComponent').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        console.log(button);
+        var idAccountLaunch = button.data('id') 
+        var nameAccountLaunch = button.data('name')
+        console.log(nameAccountLaunch)
+        var typeAccountLaunch = ""; 
+        var modal = $(this)
+        if(button.data('type') == 1) {
+            typeAccountLaunch = "Receita"
+        }else{
+            typeAccountLaunch = "Despesa"
+        }
+        modal.find('#nameAccountDeleteModal').html('Conta: '+nameAccountLaunch);
+        modal.find('#typeAccountDeleteModal').html('Tipo: '+typeAccountLaunch);
+        modal.find('#idAccountLaunch').val(idAccountLaunch);
+    })
+    // let data = packForm('#edit-form-edit-account-launch');
+    $('#edit-form-edit-account-launch').on('click', function (event) {
+        let idEdit = $('#modalAccountLaunchId').val();
+        var form = $("#form-edit-account-launch").serialize();
+        SearaAjax.put('/launch/account/' + idEdit, form)
+        .then(function (response) {
+            notify.response(response);
+            $("#account-launch-table").DataTable().ajax.reload();
+            $('#modalEditAccountLaunch').modal('hide');
+        })
+        .fail(function (jqXHR) {
+            notify.response();
+        })
+        .always(function () {
+            SearaLoader.hideModal();
+        })
+    });
+});
---
+++ resources/views/launch/index.blade.php
@@ -0,0 +1,106 @@
+@extends('layouts.blank')
+@push('stylesheets')
+<!-- Example -->
+<!--<link href=" <link href="{{ asset("css/myFile.min.css") }}" rel="stylesheet">" rel="stylesheet">-->
+@endpush
+@section('main_container')
+<!-- page content -->
+<div class="right_col" role="main">
+   
+    <div class="row">
+        @include('msg.message')
+        <div class="col-md-12">
+            <div class="x_panel">
+                <div class="x_title">
+                    <h2>Conta <small>Criar conta de lançamento para o caixa</small></h2>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                    <form class="form-horizontal form-label-left" id="form-account-launch">
+                        <div class="form-group row">
+                            <div class="col-md-3">
+                                <label class="col-form-label">Tipo da conta</label>
+                                <select name="accountlaunch_type" id="" class="form-control">
+                                    <option value="">--Selecione--</option>
+                                    <option value="1">Receitas</option>
+                                    <option value="2">Despesa</option>
+                                </select>
+                            </div>
+                            <div class="col-md-9">
+                                <label class="col-form-label">Nome da conta</label>
+                                <input type="text" name="accountlaunch_name" class="form-control">
+                            </div>
+                            <div class="col-sm-12">
+                                <label for="">Histórico da conta</label>
+                                <div class="input-group">
+                                    <input type="text" name="accountlaunch_history" id="" class="form-control">
+                                    <span class="input-group-btn">
+                                    <button type="button" class="btn btn-primary" id="btn-save-account-launch">Salvar conta</button>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="divider-dashed"></div>
+                        <input type="hidden" name="accountlaunch_id_user" value="{{Auth::user()->id}}">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="x_panel">
+        <div class="x_title">
+            <h2>Conta <small>Todas as contas</small></h2>
+            <div class="clearfix"></div>
+        </div>
+        <div class="x_content">
+            <div class="right-col">
+                <table id="account-launch-table" class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>Tipo da Conta</th>
+                            <th>Nome da Conta</th>
+                            <th>Histórico</th>
+                            <th>Criada em</th>
+                            <th>Criada por</th>
+                            <th>Ação</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /page content -->
+
+@include('modals.launch.modal_account_launch')
+@component('components.modal_delete_comp')
+<form action="{{url('launch/account/delete')}}" method="POST">
+    {!! csrf_field() !!}
+    <p>
+        <h4 class="text-danger">
+            Deseja realmente excluir essa conta do movimento de Caixa?
+        </h4>
+    </p>
+    <p>
+        <h4 id="nameAccountDeleteModal">Conta: </h4>
+        <h4 id="typeAccountDeleteModal">Tipo da Conta: </h4>
+        
+    </p>
+    <input type="text" name="id" id="idAccountLaunch">
+    <input type="text" name="table" value="account_launches">
+    <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Sair</button>
+        <button type="submit" class="btn btn-danger"> EXCLUIR </button>
+    </div>
+</form>
+@endcomponent
+@endsection
+@push('stylesheets')
+{{-- 
+<link rel="stylesheet" type="text/css" href="{{asset('css/receipt.min.css')}}">
+--}}
+@endpush
+@push('scripts')
+<script type="text/javascript" language="javascript" src="{{asset('js/launch/account_launch.min.js')}}"></script>
+{{-- <script type="text/javascript" language="javascript" src="{{asset('js/receipt-common.min.js')}}"></script> --}}
+@endpush
