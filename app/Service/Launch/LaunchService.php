3cd66ad8b52b0cd94f879bc1287e06ff0036a7cd
JuinEsp <franciscoanto@gmail.com>
2022-09-30 19:09:39 -0300

    Refatorando

--- app/Service/Launch/LaunchService.php
+++ app/Service/Launch/LaunchService.php
@@ -1,11 +1,13 @@
 <?php
+
 namespace Seara\Service\Launch;
 
 use Seara\Entry;
 use Seara\AccountBank;
 use DB;
 
-class LaunchService {
+class LaunchService
+{
 
     /**
      * Retorna o saldo atual dos lancamento/Caixa interno
@@ -16,46 +18,49 @@ class LaunchService {
     static public function getBoxInternal($idCompany)
     {
         $balance  = self::getCountValueLaunch($idCompany, 'Receita', 1);
-        // $expense = self::getCountValueLaunch($idCompany, 'Despesa', 1);
         return $balance;
     }
 
-   /**
-    * Retorna o valor dos lancamentos dependendo do tipo de conta
-    *
-    * @param [type] $idCompany
-    * @param [type] $type
-    * @return void
-    */
+    /**
+     * Retorna o valor dos lancamentos dependendo do tipo de conta
+     *
+     * @param [type] $idCompany
+     * @param [type] $type
+     * @return void
+     */
     static function getCountValueLaunch($idCompany, $type, $transaction)
     {
         // DB::enableQueryLog();
-        $entry  = Entry::join('account_launches','entries.entries_id_account','=','account_launches.id')
-        ->join('account_types', 'account_launches.accountlaunch_type', '=', 'account_types.id')
-        // ->where('account_types.account_types_name','=', $type)
-        ->where('entries.entries_id_company', '=', $idCompany)        
-        //->where('entries.transaction_id', '=', $transaction)        
-        ->select('account_launches.accountlaunch_type', 'account_types.id',
-        'account_types.account_types_name',
-        'entries.entries_id_account', 'entries.entries_id_company','entries.entries_value',
-        'entries.entries_id as idEntry', 'account_types.id as idAccountType')->get();
+        $entry  = Entry::join('account_launches', 'entries.entries_id_account', '=', 'account_launches.id')
+            ->join('account_types', 'account_launches.accountlaunch_type', '=', 'account_types.id')
+            // ->where('account_types.account_types_name','=', $type)
+            ->where('entries.entries_id_company', '=', $idCompany)
+            //->where('entries.transaction_id', '=', $transaction)        
+            ->select(
+                'account_launches.accountlaunch_type',
+                'account_types.id',
+                'account_types.account_types_name',
+                'entries.entries_id_account',
+                'entries.entries_id_company',
+                'entries.entries_value',
+                'entries.entries_id as idEntry',
+                'account_types.id as idAccountType'
+            )->get();
         //retorna o valor encontrado
         //dd( DB::getQueryLog());
         $receita = 0;
         $despesa = 0;
-foreach ($entry as $key => $value) {
-    // echo $value['account_types_name'];
-    
-    if($value['account_types_name'] == 'Receita'){
-        $receita = ($receita + $value['entries_value']);
-    }else{
-        $despesa = ($despesa + $value['entries_value']);
-    }
-}
-// dump($receita);
-// dump($despesa);
-$saldo = ($receita - $despesa);
-// dump($saldo);
+        foreach ($entry as $key => $value) {
+            switch ($value['account_types_name']) {
+                case 'Receita':
+                    $receita = ($receita + $value['entries_value']);
+                    break;
+                case 'Despesa':
+                    $despesa = ($despesa + $value['entries_value']);
+                    break;
+            }
+        }
+        $saldo = ($receita - $despesa);
         // empty($value) ? $value = 0 : $value = $value->sum('entries_value');
         return $saldo;
     }
@@ -68,7 +73,6 @@ $saldo = ($receita - $despesa);
      */
     static public function getBoxBank($idCompany)
     {
-       return AccountBank::where('company_id', '=', $idCompany)->sum('balance');
+        return AccountBank::where('company_id', '=', $idCompany)->sum('balance');
     }
-
 }
