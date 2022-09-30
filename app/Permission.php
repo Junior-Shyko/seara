8ed3ca8e31815d20acb7bd459ee296213deabe70
JuinEsp <franciscoanto@gmail.com>
2022-09-30 19:07:24 -0300

    ADD funcao de verificação de acesso do usuario

--- app/Permission.php
+++ app/Permission.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
-    const superAdmin = 1;//se é um superAdmin dentro da tabela permissions
+    const superAdmin = 'superAdmin';//se é um superAdmin dentro da tabela permissions
     const admin = 2;//se é um admin dentro da tabela permissions
 
     public function profiles_permission()
@@ -27,7 +27,32 @@ class Permission extends Model
      */
     static public function verifyAccess($user, $permission)
     {
-        return $user->hasPermissionTo(Permission::find($permission)->name);
+        return $user->hasRole($permission);
+    }
 
+    /**
+     * Verifica a permissão de cada usuário
+     * Se o usuário não foi um superAdmin o mesmo é verificado
+     * se o id_company dele bate com o da url
+     * @param [type] $idCompany
+     * @param [type] $user
+     * @return void
+     */
+    static public function verifyPermission($idCompany, $user)
+    {
+        $access = false;
+        //RECEBENDO OS PAPEIS DO USUÁRIO
+        $role = $user->getRoleNames();
+        //LOOP NA COLLECTION PARA COMPARAR SE O USUARIO PERTENCIA A IGREJA
+        foreach ($role as $roles) {
+            if($roles !== 'superAdmin'){
+                if($user->user_id_company == $idCompany)
+                {
+                    $access = true;
+                }
+            }
+        }
+
+        return $access;
     }
 }
