a3993dc9ae2beac0a1ca59d0f590d1efc6cd2c4a
Junior-Oliveira <franciscoanto@gmail.com>
2023-05-23 22:56:06 -0300

    Script de criação de view

--- database/views/debt_report_view.sql
+++ database/views/debt_report_view.sql
@@ -15,3 +15,27 @@ select
     company_id as company_id,
     id as id
 from receivable;
+
+CREATE OR REPLACE
+ALGORITHM = UNDEFINED VIEW `seara`.`debt_report_view` AS
+select
+    `seara`.`payment`.`amount` AS `amount`,
+    `seara`.`payment`.`payment_date` AS `effective_date`,
+    'PAGAMENTO' AS `description`,
+    `seara`.`receivable`.`company_id` AS `company_id`,
+    `seara`.`payment`.`id` AS `id`
+from
+    ((`seara`.`payment_part`
+left join `seara`.`receivable` on
+    ((`seara`.`payment_part`.`receivable_id` = `seara`.`receivable`.`id`)))
+left join `seara`.`payment` on
+    ((`seara`.`payment_part`.`payment_id` = `seara`.`payment`.`id`)))
+union all
+select
+    -(`seara`.`receivable`.`amount`) AS `amount`,
+    `seara`.`receivable`.`due_date` AS `effective_date`,
+    `seara`.`receivable`.`description` AS `description`,
+    `seara`.`receivable`.`company_id` AS `company_id`,
+    `seara`.`receivable`.`id` AS `id`
+from
+    `seara`.`receivable`;
--- database/views/receivable_view.sql
+++ database/views/receivable_view.sql
@@ -16,3 +16,40 @@ from `receivable`
          inner join `income_category` on `receivable`.`income_category_id` = `income_category`.`id`
          inner join `account` on `receivable`.`account_id` = `account`.`id`
          left join `companies` on `receivable`.`company_id` = `companies`.`company_id`;
+
+
+         -- seara.receivable_view source
+
+CREATE OR REPLACE
+ALGORITHM = UNDEFINED VIEW `seara`.`receivable_view` AS
+select
+    `seara`.`receivable`.`id` AS `id`,
+    `seara`.`receivable`.`due_date` AS `due_date`,
+    (
+    select
+        cast(`seara`.`payment_part`.`payment_date` as date)
+    from
+        `seara`.`payment_part`
+    where
+        (`seara`.`payment_part`.`receivable_id` = `seara`.`receivable`.`id`)
+    order by
+        `seara`.`payment_part`.`payment_date` desc
+    limit 1) AS `payment_date`,
+    `seara`.`receivable`.`description` AS `description`,
+    `seara`.`income_category`.`name` AS `category`,
+    `seara`.`account`.`name` AS `account`,
+    coalesce((select sum(`seara`.`payment_part`.`amount`) from `seara`.`payment_part` where (`seara`.`payment_part`.`receivable_id` = `seara`.`receivable`.`id`)), 0) AS `paid_amount`,
+    `seara`.`receivable`.`amount` AS `amount`,
+    coalesce(`seara`.`companies`.`company_name`, `seara`.`companies`.`company_fantasy`) AS `customer`,
+    `seara`.`companies`.`company_manager` AS `manager`,
+    `seara`.`companies`.`company_id` AS `company_id`,
+    `seara`.`receivable`.`sequence_number` AS `sequence_number`,
+    `seara`.`receivable`.`sequence_count` AS `sequence_count`
+from
+    (((`seara`.`receivable`
+join `seara`.`income_category` on
+    ((`seara`.`receivable`.`income_category_id` = `seara`.`income_category`.`id`)))
+join `seara`.`account` on
+    ((`seara`.`receivable`.`account_id` = `seara`.`account`.`id`)))
+left join `seara`.`companies` on
+    ((`seara`.`receivable`.`company_id` = `seara`.`companies`.`company_id`)));
