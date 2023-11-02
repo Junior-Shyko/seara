5401d36c0fdc1068c01b5fb4085eb2866da79b72
Junior-Shyko <franciscoanto@gmail.com>
2023-11-02 12:11:37 -0300

    :tada: Iniciado backend de conf de caixa
    Criado model, controller, migration e request

---
+++ app/Http/Controllers/SettingsBoxController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Seara\Http\Controllers;
+
+use Seara\SettingsBox;
+use Illuminate\Http\Request;
+
+class SettingsBoxController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \Seara\SettingsBox  $settingsBox
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SettingsBox $settingsBox)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \Seara\SettingsBox  $settingsBox
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SettingsBox $settingsBox)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Seara\SettingsBox  $settingsBox
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SettingsBox $settingsBox)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Seara\SettingsBox  $settingsBox
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SettingsBox $settingsBox)
+    {
+        //
+    }
+}
