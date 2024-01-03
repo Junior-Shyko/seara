12b6ecd758d5ae3926ddcc26671bc228877cf388
Junior-Shyko <franciscoanto@gmail.com>
2024-01-03 19:57:58 -0300

    Comentado botão de fechar caixa com data estabelecida

--- resources/views/setting_box/edit.blade.php
+++ resources/views/setting_box/edit.blade.php
@@ -127,74 +127,74 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="panel panel-default">
-                                        <div class="panel-heading" role="tab" id="headingTwo">
-                                            <h4 class="panel-title">
-                                                <a class="collapsed btn btn-default" role="button" data-toggle="collapse"
-                                                    data-parent="#accordion" href="#collapseTwo" aria-expanded="false"
-                                                    aria-controls="collapseTwo">
-                                                    Fechar com edição de data
-                                                </a>
-                                            </h4>
-                                        </div>
-                                        <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel"
-                                            aria-labelledby="headingTwo">
-                                            <div class="panel-body">
-                                                <p class="text-center">
-                                                    Essa opção, possibilita de fazer alteração da data do tempo/espaço do caixa.
-                                                </p>
-                                                <form id="form-edit-box" data-parsley-validate="" method="POST"
-                                                    action="{{ url('caixa') }}" class="form-horizontal form-label-left"
-                                                    novalidate="">
-                                                    <div class="item form-group">
-                                                        <label class="col-form-label col-md-3 col-sm-3 label-align"
-                                                            for="first-name">Data de início
-                                                            <span class="required">*</span>
-                                                        </label>
-                                                        <div class="col-md-6 col-sm-6 ">
-                                                            <input id="date_open_box" class="form-control"
-                                                                placeholder="dd/mm/aaaa" type="text" required="required"
-                                                                name="date_open"
-                                                                value="{{ \Carbon\Carbon::parse($setBox->date_open)->format('d/m/Y H:i:d') }}">
-                                                        </div>
-                                                    </div>
-                                                    <div class="item form-group">
-                                                        <label class="col-form-label col-md-3 col-sm-3 label-align"
-                                                            for="last-name">Data final
-                                                            <span class="required">*</span>
-                                                        </label>
-                                                        <div class="col-md-6 col-sm-6 ">
-                                                            <input id="date_close_box_edit"
-                                                                class="date-picker form-control" placeholder="dd/mm/aaaa"
-                                                                type="text" name="date_close">
-                                                            <p class="text-muted" style="color: darkgoldenrod;">Para
-                                                                fechar o caixa, basta
-                                                                preencher a data do fechamento.</p>
-                                                        </div>
-                                                    </div>
+{{--                                    <div class="panel panel-default">--}}
+{{--                                        <div class="panel-heading" role="tab" id="headingTwo">--}}
+{{--                                            <h4 class="panel-title">--}}
+{{--                                                <a class="collapsed btn btn-default" role="button" data-toggle="collapse"--}}
+{{--                                                    data-parent="#accordion" href="#collapseTwo" aria-expanded="false"--}}
+{{--                                                    aria-controls="collapseTwo">--}}
+{{--                                                    Fechar com edição de data--}}
+{{--                                                </a>--}}
+{{--                                            </h4>--}}
+{{--                                        </div>--}}
+{{--                                        <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel"--}}
+{{--                                            aria-labelledby="headingTwo">--}}
+{{--                                            <div class="panel-body">--}}
+{{--                                                <p class="text-center">--}}
+{{--                                                    Essa opção, possibilita de fazer alteração da data do tempo/espaço do caixa.--}}
+{{--                                                </p>--}}
+{{--                                                <form id="form-edit-box" data-parsley-validate="" method="POST"--}}
+{{--                                                    action="{{ url('caixa') }}" class="form-horizontal form-label-left"--}}
+{{--                                                    novalidate="">--}}
+{{--                                                    <div class="item form-group">--}}
+{{--                                                        <label class="col-form-label col-md-3 col-sm-3 label-align"--}}
+{{--                                                            for="first-name">Data de início--}}
+{{--                                                            <span class="required">*</span>--}}
+{{--                                                        </label>--}}
+{{--                                                        <div class="col-md-6 col-sm-6 ">--}}
+{{--                                                            <input id="date_open_box" class="form-control"--}}
+{{--                                                                placeholder="dd/mm/aaaa" type="text" required="required"--}}
+{{--                                                                name="date_open"--}}
+{{--                                                                value="{{ \Carbon\Carbon::parse($setBox->date_open)->format('d/m/Y H:i:d') }}">--}}
+{{--                                                        </div>--}}
+{{--                                                    </div>--}}
+{{--                                                    <div class="item form-group">--}}
+{{--                                                        <label class="col-form-label col-md-3 col-sm-3 label-align"--}}
+{{--                                                            for="last-name">Data final--}}
+{{--                                                            <span class="required">*</span>--}}
+{{--                                                        </label>--}}
+{{--                                                        <div class="col-md-6 col-sm-6 ">--}}
+{{--                                                            <input id="date_close_box_edit"--}}
+{{--                                                                class="date-picker form-control" placeholder="dd/mm/aaaa"--}}
+{{--                                                                type="text" name="date_close">--}}
+{{--                                                            <p class="text-muted" style="color: darkgoldenrod;">Para--}}
+{{--                                                                fechar o caixa, basta--}}
+{{--                                                                preencher a data do fechamento.</p>--}}
+{{--                                                        </div>--}}
+{{--                                                    </div>--}}
 
-                                                    <div class="ln_solid"></div>
-                                                    <div class="item form-group">
+{{--                                                    <div class="ln_solid"></div>--}}
+{{--                                                    <div class="item form-group">--}}
 
-                                                        <div class="col-md-6">
+{{--                                                        <div class="col-md-6">--}}
 
-                                                        </div>
-                                                        <div class="col-md-6 col-sm-6 offset-md-3">
-                                                            <input type="hidden" name="id_user_open"
-                                                                value="{{ Auth::user()->id }}">
-                                                            <button type="button"
-                                                                onClick="updateSettingBox({{ $setBox->id }} , 'form')"
-                                                                class="btn btn-danger pull-right"
-                                                                title="Altera e/ou fecha o caixa">
-                                                                <i class="fa fa-money"></i> Fechar Caixa
-                                                            </button>
-                                                        </div>
-                                                    </div>
-                                                    <div class="ln_solid"></div>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
+{{--                                                        </div>--}}
+{{--                                                        <div class="col-md-6 col-sm-6 offset-md-3">--}}
+{{--                                                            <input type="hidden" name="id_user_open"--}}
+{{--                                                                value="{{ Auth::user()->id }}">--}}
+{{--                                                            <button type="button"--}}
+{{--                                                                onClick="updateSettingBox({{ $setBox->id }} , 'form')"--}}
+{{--                                                                class="btn btn-danger pull-right"--}}
+{{--                                                                title="Altera e/ou fecha o caixa">--}}
+{{--                                                                <i class="fa fa-money"></i> Fechar Caixa--}}
+{{--                                                            </button>--}}
+{{--                                                        </div>--}}
+{{--                                                    </div>--}}
+{{--                                                    <div class="ln_solid"></div>--}}
+{{--                                                </form>--}}
+{{--                                            </div>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
 
                                 </div>
                             </div>
