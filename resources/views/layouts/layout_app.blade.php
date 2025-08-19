960cc6862b95832b84568e0c892b3a9339c092ce
Junior-Shyko <franciscoanto@gmail.com>
2025-08-19 00:17:29 -0300

    :lipstick: Copilando mas com problemas no layout

--- resources/views/layouts/layout_app.blade.php
+++ resources/views/layouts/layout_app.blade.php
@@ -50,6 +50,7 @@
     SearaApp.baseURL = "{{ url('')  }}";
   </script>
 
+
   <script src="{{ asset("js/gentelella.min.js") }}"></script>
   <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
